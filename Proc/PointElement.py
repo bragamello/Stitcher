f7b4c2c3b44a0c5fb2660dd99c3d400d57f77cfa
Heitor Gessner <hmynssen@gmail.com>
2020-05-25 18:58:28 -0300

    Point Element Properties (*, mod, +, -)

--- Proc/PointElement.py
+++ Proc/PointElement.py
@@ -6,3 +6,11 @@ class Point:
 	def Update(self , otherx , othery):
 		self.x = otherx
 		self.y = othery
+	def __pow__(v, u):
+		return u.y * v.x - u.x * v.y
+	def mod(self):
+		return pow(self.x ** 2 + self.y ** 2, 0.5)
+	def __add__(v, u):
+		return Point(v.x + u.x, v.y + u.y)
+	def __sub__(v, u):
+		return Point(v.x - u.x, v.y - u.y)
